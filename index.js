803a9bb79989d2ebb829d082ece9ab6dc22dae35
Samuel Hernandez <91435847+kusanx@users.noreply.github.com>
2022-07-18 17:04:08 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let usuario = parseInt(prompt("Ingresa un nombre de usuario mayor de 6 caracteres"));
-
-while(usuario.length < 6){
-    alert("El nombre de usuario es menor a 6 caracteres");
-    prompt("Ingresa un nombre de usuario");
-}
-
-alert("Bienvenido!");
