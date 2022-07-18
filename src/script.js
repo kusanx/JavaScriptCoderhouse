8c7a484d4b467351827f015c18d0e2e92f406484
Samuel Hernandez <91435847+kusanx@users.noreply.github.com>
2022-07-18 17:07:57 -0500

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,1 +1,23 @@
+const loginForm = document.getElementById("form");
+const botonLogin = document.getElementById("login-form-submit");
 
+botonLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    const matricula = form.matricula.value;
+    const usuario = form.usuario.value;
+    const password = form.password.value;
+    
+    const matriculasRegistradas = ["202201", "202202", "202203"];
+    const usuariosRegistrados = ["profesor1", "profesor2", "profesor3"];
+    const passwordRegistrados = ["12345", "67890", "coderhouse"];
+    
+    for (var i = 0; i < matriculasRegistradas.length; i++) {
+        if (matricula == matriculasRegistradas[i] && usuario == usuariosRegistrados[i] && password == passwordRegistrados[i]) {
+          alert('Inicio de sesion correcto!')
+          window.location.href= "plataforma.html";
+          break;
+        } else {
+          alert('Error en el inicio de sesion, revisa tus datos e intenta nuevamente!')
+        }
+      }
+    })
